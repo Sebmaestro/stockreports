6a26046ae94b496b7a5aabf8c99b9c7292c6d29e
sebbe <sebbe@DESKTOP-DHCGTGB>
2025-05-20 23:19:24 +0200

    Fixing warnings

--- backend/src/main/java/com/example/backend/controller/StockController.java
+++ backend/src/main/java/com/example/backend/controller/StockController.java
@@ -1,16 +1,10 @@
 package com.example.backend.controller;
 
-import com.example.backend.model.User;
+
 import com.example.backend.service.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
